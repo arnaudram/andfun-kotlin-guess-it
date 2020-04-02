44789b683ff1732de92c31a8e0a2e24cb5883e30
arnaudram <52896739+arnaudram@users.noreply.github.com>
2020-04-02 08:51:10 -0700

    Step.09-Solution-Add-LiveData-Data-Binding.#Udacity

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -58,15 +58,13 @@ class ScoreFragment : Fragment() {
                 .get(ScoreViewModel::class.java)
 
         binding.scoreViewModel = viewModel
-        // TODO (05) Call binding.setLifecycleOwner and remove the score observer
 
+          binding.setLifecycleOwner { lifecycle }
         // Add observer for score
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
+
 
         // Navigates back to title when button is pressed
-        viewModel.eventPlayAgain.observe(this, Observer { playAgain ->
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
             if (playAgain) {
                 findNavController().navigate(ScoreFragmentDirections.actionRestart())
                 viewModel.onPlayAgainComplete()
